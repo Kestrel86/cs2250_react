2104b4c5229c2661103e6d5081454b6161184d84
Kestrel86 <valdezandrew20@gmail.com>
2023-11-23 00:50:13 -0800

    added pages

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 
-import App from "./App";
+import App from "./pages/TicTacToe";
+import Home from "./pages/Home";
+import API from "./pages/API";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
