8f036b42b88362646b2571c27aba12d1b27bda79
Kestrel86 <valdezandrew20@gmail.com>
2023-11-23 12:07:43 -0800

    displayed navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./styles.css";
 
 export default function Navbar() {
   return (
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/api">API</Link>
-      </li>
-      <li>
-        <Link to="/tictactoe">Tictactoe</Link>
-      </li>
-    </ul>
+    <nav className="nav">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/api">API</Link>
+        </li>
+        <li>
+          <Link to="/tictactoe">Tictactoe</Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
